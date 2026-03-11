aa3c95c0b8c00595e48a56ee3bab2df58f551c08
Marquis Verino <marquisverino12@gmail.com>
2026-03-11 23:55:24 +0700

    Add files via upload
    # 1

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+
+// SECTION 2 -- VARIABLES
+int main() {
+    int age = 21;
+    int year = 2026;
+    int days = 30;
+    std::cout << age << '\n';
+    
+    double price = 10.99;
+    double gpa = 3.99;
+    double temp = 21.5;
+    std::cout << price << "\n";
+
+    char grade = 'A';
+    char initial = 'E';
+    char currency = '$';
+    std::cout << initial << '\n';
+
+    bool student = true;
+    bool powered = false;
+    bool available = true;
+    std::cout << powered << '\n';
+
+    std::string name = "Bro";
+    std::string day = "Monday";
+    std::string food = "Banana";
+    std::string addres = "Jamaica St 17";
+    std::cout << name << '\n';
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You're " << age << " years old";
+    return 0;
+}
+
+// int main() {
+
+//     int x = 5;
+//     int y = 6;
+
+//     int sum = x + y;
+    
+//     std::cout << x << "\n";
+//     std::cout << y << "\n";
+//     std::cout << x << "\n";
+
+//     return 0;
+// }
+
+
+// SECTION 1 -- PRINT & COMMENTS
+// int main() {
+
+//     // This is a comment!
+
+//     /*
+//         This is a
+//         huge comment!
+//     */
+
+//     std::cout << "I like banana!" << "\n";
+//     std::cout << "It's very nutritious!" << "\n";
+//     return 0;
+// }
